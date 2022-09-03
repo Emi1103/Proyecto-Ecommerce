f91794a2ad377b6c7951c1ab553558afedf184d9
Emi1103 <emilce.correa@outlook.com>
2022-09-03 16:28:29 -0300

    modified

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ let total = 0;
 function add (product,price) {
   console.log(product,price);
   products.push(product);
+
+  swal("Prenda agregada!", "Ya puedes pasar por caja!", "success");
+
   total = total + price;
-  document.getElementById('checkout').innerHTML = `Pagar $${total}`;
+  if (document.getElementById('checkout').innerHTML = `Pagar $${total}`);
+  setTimeout(() => {
+    // Eliminar mensaje
+    document.getElementById('checkout').innerHTML = 'Pagar';
+}, 5000);
 }
 
 function pay(){
-  // console.log(product,price);
-  window.alert(products.join(', \n'));
+  
+  swal({
+    title: "Segura que quieres finalizar la compra?",
+    text: (products.join(', \n')),
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Listo! Pagaste con dinero en cuenta!", {
+        icon: "success",
+      });
+      
+    } else {
+      swal("Sigue agregando más prendas ❤️!");
+    }
+  });
+
+  
+
 }
